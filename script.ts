33d5a8b901bdd3c39a7aeed1abfeb401e6303660
Dom Gaulton <domgaulton@gmail.com>
2022-11-16 15:26:04 +0000

    first commit

---
+++ script.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+
+//   const user = await prisma.user.create({
+//     data: {
+//       name: 'Dom',
+//       email: 'dom@prisma.io',
+//     },
+//   })
+//   console.log(user)
+
+// const users = await prisma.user.findMany()
+// console.log(users)
+
+const user = await prisma.user.create({
+    data: {
+      name: 'Test',
+      email: 'test@prisma.io',
+      posts: {
+        create: {
+          title: 'Hello World',
+        },
+      },
+    },
+  })
+  console.log(user)
+
+// const usersWithPosts = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//     },
+//   })
+//   console.dir(usersWithPosts, { depth: null })
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
